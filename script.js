03f8e5cb0f9566a38bef5a648720e84e6bff0301
Banyu_sans <banyuarfa1122@gmail.com>
2024-08-19 07:15:45 -0700

    v0.1.0

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
 const header = document.querySelector("header");
+const venus = document.querySelector(".venus");
+const saturn = document.querySelector(".saturn");
+const constellation = document.querySelector(".constellation");
+const fallingStar = document.querySelector(".falling-star");
+const birds = document.querySelector(".birds");
+const students = document.querySelector(".students");
+
 window.addEventListener("scroll", () => {
-  if (window.scrollY > 0 && window.scrollY < 1700) {
+  const scrollValue = window.scrollY;
+
+  // Parallax Events
+  venus.style.marginTop = `-${scrollValue * 0.2}px`;
+  fallingStar.style.marginRight = `${scrollValue * 0.7}px`;
+  fallingStar.style.marginTop = `${scrollValue * 0.7}px`;
+  saturn.style.marginTop = `-${scrollValue * 0.3}px`;
+  constellation.style.marginBottom = `${scrollValue * 0.1}px`;
+  students.style.marginRight = `${scrollValue * 0.1}px`;
+  birds.style.marginTop = `${scrollValue * 0.1}px`;
+  birds.style.marginRight = `${scrollValue * 0.2}px`;
+
+  // NavBar Styling
+  if (scrollValue > 0 && scrollValue < 1700) {
     header.classList.add("scrolled");
     header.classList.remove("scrolled-more");
-  } else if (window.scrollY > 1700) {
+  } else if (scrollValue > 1700) {
     header.classList.add("scrolled-more");
     header.classList.remove("scrolled");
   } else {
